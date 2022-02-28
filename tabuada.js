fc94be05902f1a2acd9e8eae9194fb8728a75046
Sergio <79654380+SergioPaulo-ft@users.noreply.github.com>
2022-02-28 01:45:54 -0300

    Update tabuada.js

--- tabuada.js
+++ tabuada.js
@@ -1,5 +1,5 @@
 let enter = document.querySelector("#numero") //input
-let addTabuada = document.querySelector("#adicionar") //botão 
+let addTabuada = document.querySelector("#adicionar") //botao
 let clear = document.querySelector('#limpar') //botão limpar tabuada
 let res = document.querySelector('#res') // resposta
 let tabuadaClick = ""
@@ -9,30 +9,33 @@ let tabuadaEnter = ""
 addTabuada.addEventListener("click", function click(event) {
     let numero = parseInt(document.querySelector("#numero").value)
     for (i = 0; i <= 10; i++) {
-        tabuadaClick += `${numero} x ${i} = ${numero * i} <br>`
-    }
-    !isNaN(numero) && (res.innerHTML = tabuadaClick) //desabilita NaN
-       
+        if(isNaN(numero)){
+            res.innerHTML = ``
+            }else{
+            tabuadaClick += `${numero} x ${i} = ${numero * i} <br>`
+            }
+        }
+        
+        res.innerHTML +=  tabuadaClick  + "<br>"
 })
 //adicionando tabuada com tecla "Enter"
 enter.addEventListener("keypress", function (e) {
     let numero = parseInt(document.querySelector("#numero").value)
-
     if (e.key === "Enter") {
-        let nonumber = isNaN(numero)
-        //tabuadaEnter.indexOf(NaN)
-            for (i = 0; i <= 10; i++) {
-                tabuadaEnter += `${numero} x ${i} = ${numero * i} <br>`
+        for (i = 0; i <= 10; i++) {
+            if(isNaN(numero)){
+                res.innerHTML = ``
+                }else{
+                tabuadaClick += `${numero} x ${i} = ${numero * i} <br>`
+                }
             }
-            (isNaN(numero) && (res.innerHTML = 'NaN encontrado'))
-           && (nonumber = 0)
-            !isNaN(numero) && (res.innerHTML = tabuadaEnter)
+            
+            res.innerHTML +=  tabuadaClick  + "<br>"
     }
 })
-
 //limpando tabuada
 clear.addEventListener("click", limpar = (clear) => {
     res.innerHTML = ""
     tabuadaEnter = ""
     tabuadaClick = ""
-}) 
+})
