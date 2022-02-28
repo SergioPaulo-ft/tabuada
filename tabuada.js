401e29897110532767d99fc05c18d0454712d7a6
Sergio <79654380+SergioPaulo-ft@users.noreply.github.com>
2022-02-28 02:08:38 -0300

    Update tabuada.js

--- tabuada.js
+++ tabuada.js
@@ -9,28 +9,26 @@ let tabuadaEnter = ""
 addTabuada.addEventListener("click", function click(event) {
     let numero = parseInt(document.querySelector("#numero").value)
     for (i = 0; i <= 10; i++) {
-        if(isNaN(numero)){
+        if (isNaN(numero)) {
             res.innerHTML = ``
-            }else{
+        } else {
             tabuadaClick += `${numero} x ${i} = ${numero * i} <br>`
-            }
         }
-        
-        res.innerHTML +=  tabuadaClick  + "<br>"
+    }
+    res.innerHTML += tabuadaClick + "<br>"
 })
 //adicionando tabuada com tecla "Enter"
 enter.addEventListener("keypress", function (e) {
     let numero = parseInt(document.querySelector("#numero").value)
     if (e.key === "Enter") {
         for (i = 0; i <= 10; i++) {
-            if(isNaN(numero)){
+            if (isNaN(numero)) {
                 res.innerHTML = ``
-                }else{
+            } else {
                 tabuadaClick += `${numero} x ${i} = ${numero * i} <br>`
-                }
             }
-            
-            res.innerHTML +=  tabuadaClick  + "<br>"
+        }
+        res.innerHTML += tabuadaClick + "<br>"
     }
 })
 //limpando tabuada
